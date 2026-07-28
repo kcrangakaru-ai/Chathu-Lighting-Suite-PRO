bd2c0f38923b87d151da20459e08bd086f4a2f7a
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-29 04:55:13 +0530

    refactor: enhance logger with debug and success support
    Add debug, success, and banner logging while improving message formatting.Add debug, success, and banner logging while improving message formatting.

--- src/Logger.lua
+++ src/Logger.lua
@@ -1,19 +1,66 @@
 --------------------------------------------------
+-- Chathu Lighting Suite PRO
 -- Logger
 --------------------------------------------------
 
 local Logger = {}
 
+--------------------------------------------------
+-- Information
+--------------------------------------------------
+
 function Logger.Info(msg)
-    gma.echo("[CLS] "..msg)
+    gma.echo("[INFO] " .. tostring(msg))
 end
+
+--------------------------------------------------
+-- Warning
+--------------------------------------------------
 
 function Logger.Warn(msg)
-    gma.echo("[WARNING] "..msg)
+    gma.echo("[WARNING] " .. tostring(msg))
 end
 
+--------------------------------------------------
+-- Error
+--------------------------------------------------
+
 function Logger.Error(msg)
-    gma.echo("[ERROR] "..msg)
+    gma.echo("[ERROR] " .. tostring(msg))
+end
+
+--------------------------------------------------
+-- Success
+--------------------------------------------------
+
+function Logger.Success(msg)
+    gma.echo("[SUCCESS] " .. tostring(msg))
+end
+
+--------------------------------------------------
+-- Debug
+--------------------------------------------------
+
+function Logger.Debug(msg)
+
+    if Config and Config.Debug then
+        gma.echo("[DEBUG] " .. tostring(msg))
+    end
+
+end
+
+--------------------------------------------------
+-- Banner
+--------------------------------------------------
+
+function Logger.Banner()
+
+    Logger.Info("===================================")
+    Logger.Info(Config.Name)
+    Logger.Info("Version : " .. Config.Version)
+    Logger.Info("Author  : " .. Config.Author)
+    Logger.Info("===================================")
+
 end
 
 return Logger
