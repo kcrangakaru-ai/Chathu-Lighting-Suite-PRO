ea420cef16e4e263fdea8e1ef962765cb472e017
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-28 16:59:53 +0530

    Update Logger.lua

--- src/Logger.lua
+++ src/Logger.lua
@@ -1,23 +1,19 @@
 local Logger = {}
 
 function Logger.Info(msg)
-    gma.echo("[INFO] " .. msg)
+    gma.echo("[INFO] "..msg)
 end
 
 function Logger.Warning(msg)
-    gma.echo("[WARNING] " .. msg)
+    gma.echo("[WARNING] "..msg)
 end
 
 function Logger.Error(msg)
-    gma.echo("[ERROR] " .. msg)
+    gma.echo("[ERROR] "..msg)
 end
 
 function Logger.Debug(msg)
-
-    if true then
-        gma.echo("[DEBUG] " .. msg)
-    end
-
+    gma.echo("[DEBUG] "..msg)
 end
 
 return Logger
