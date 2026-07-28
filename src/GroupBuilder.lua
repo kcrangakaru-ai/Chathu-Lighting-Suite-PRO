7c6cf22703364a9f9ab0431bd4dde8ab722c9022
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-28 21:04:32 +0530

    feat: add group builder module

---
+++ src/GroupBuilder.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------
+-- Group Builder
+--------------------------------------------------
+
+local GroupBuilder = {}
+
+function GroupBuilder.Build()
+
+    gma.echo("Group Builder Loaded")
+
+end
+
+return GroupBuilder
