43e07afa5317a459daad96cde016e7b5bb7f5fa0
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-28 15:30:30 +0530

    Add Main.lua

---
+++ src/Main.lua
@@ -0,0 +1,11 @@
+local function Main()
+
+    gma.echo("================================")
+    gma.echo("Chathu Lighting Suite PRO")
+    gma.echo("Version 1.0.0")
+    gma.echo("Plugin Loaded Successfully")
+    gma.echo("================================")
+
+end
+
+return Main
