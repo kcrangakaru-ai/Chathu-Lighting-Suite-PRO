a49fa0b22120c60c3b58be9d43f8341379c1cd35
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-29 06:30:15 +0530

    feat: add PageBuilder module

---
+++ src/PageBuilder.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------
+-- Chathu Lighting Suite PRO
+-- Page Builder
+--------------------------------------------------
+
+local PageBuilder = {}
+
+function PageBuilder.Build()
+
+    Logger.Info("Building Pages...")
+
+    Logger.Success("Page Builder Complete")
+
+end
+
+return PageBuilder
