0cc224fd168ce6fd6e89e5936de45b32f8f98b50
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-29 06:31:01 +0530

    feat: add LayoutBuilder module

---
+++ src/LayoutBuilder.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------
+-- Chathu Lighting Suite PRO
+-- Layout Builder
+--------------------------------------------------
+
+local LayoutBuilder = {}
+
+function LayoutBuilder.Build()
+
+    Logger.Info("Building Layouts...")
+
+    Logger.Success("Layout Builder Complete")
+
+end
+
+return LayoutBuilder
