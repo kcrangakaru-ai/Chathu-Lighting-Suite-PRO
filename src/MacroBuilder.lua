c24cddf270647535b32b63fd9cf69b00ddb830dd
kcrangakaru-ai <kcrangakaru@gmail.com>
2026-07-29 06:28:31 +0530

    feat: add MacroBuilder module
    Commit new file

---
+++ src/MacroBuilder.lua
@@ -0,0 +1,63 @@
+--------------------------------------------------
+-- Chathu Lighting Suite PRO
+-- Macro Builder
+--------------------------------------------------
+
+local MacroBuilder = {}
+
+--------------------------------------------------
+-- Build Macros
+--------------------------------------------------
+
+function MacroBuilder.Build()
+
+    Logger.Info("Building Macros...")
+
+    --------------------------------------------------
+    -- Future Macro Creation
+    --------------------------------------------------
+
+    -- Store Macro
+    -- Label Macro
+    -- Add Macro Lines
+
+    Logger.Success("Macro Builder Complete")
+
+end
+
+--------------------------------------------------
+-- Return Module
+--------------------------------------------------
+
+return MacroBuilder--------------------------------------------------
+-- Chathu Lighting Suite PRO
+-- Macro Builder
+--------------------------------------------------
+
+local MacroBuilder = {}
+
+--------------------------------------------------
+-- Build Macros
+--------------------------------------------------
+
+function MacroBuilder.Build()
+
+    Logger.Info("Building Macros...")
+
+    --------------------------------------------------
+    -- Future Macro Creation
+    --------------------------------------------------
+
+    -- Store Macro
+    -- Label Macro
+    -- Add Macro Lines
+
+    Logger.Success("Macro Builder Complete")
+
+end
+
+--------------------------------------------------
+-- Return Module
+--------------------------------------------------
+
+return MacroBuilder
